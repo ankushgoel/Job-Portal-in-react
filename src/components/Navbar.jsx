4b906b722aae993cb0fccda467f4355bece7b087
Ankush Goel <goelankush1312@gmail.com>
2024-05-16 00:13:42 +0530

    updated a tag to Link tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/react.svg'
 
 const Navbar = () => {
@@ -10,33 +11,29 @@ const Navbar = () => {
                         className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
                     >
                         {/* <!-- Logo --> */}
-                        <a className="flex flex-shrink-0 items-center mr-4" href="/">
+                        <Link className="flex flex-shrink-0 items-center mr-4" to="/">
                             <img
                                 className="h-10 w-auto"
                                 src={logo}
                                 alt="Jobs Portal"
                             />
                             <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                            >Jobs Portal</span
-                            >
-                        </a>
+                            >Jobs Portal</span>
+                        </Link>
                         <div className="md:ml-auto">
                             <div className="flex space-x-2">
-                                <a
-                                    href="/"
+                                <Link
+                                    to="/"
                                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >Home</a
-                                >
-                                <a
-                                    href="/jobs"
+                                >Home</Link>
+                                <Link
+                                    to="/jobs"
                                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >Jobs</a
-                                >
-                                <a
-                                    href="/add-job"
+                                >Jobs</Link>
+                                <Link
+                                    to="/add-job"
                                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                                >Add Job</a
-                                >
+                                >Add Job</Link>
                             </div>
                         </div>
                     </div>
