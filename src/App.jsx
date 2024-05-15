a65b68adefd40f23a1dd2a25273b187fab7c3c92
Ankush Goel <goelankush1312@gmail.com>
2024-05-15 23:00:48 +0530

    dynamic job listings from json

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 function App() {
 
@@ -19,13 +20,7 @@ function App() {
       {/* <!-- Browse Jobs --> */}
       <JobListings />
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="#"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,6 +1,10 @@
 // import React from 'react'
+import { jobs } from '../jobs.json'
+import JobListing from './JobListing';
 
 const JobListings = () => {
+    // console.log(jobs);
+    const recentJobs = jobs.slice(0, 3)
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -8,6 +12,9 @@ const JobListings = () => {
                     Browse Jobs
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {recentJobs.map((job) => (
+                        <JobListing key={job.id} job={job} />
+                    ))}
                     {/* <!-- Job Listing 1 --> */}
                     <div className="bg-white rounded-xl shadow-md relative">
                         <div className="p-4">
