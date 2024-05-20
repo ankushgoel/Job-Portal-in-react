9dac64e042bc71487ec5f674b9c3b278686e0096
Ankush Goel <goelankush1312@gmail.com>
2024-05-20 23:41:21 +0530

    Send new job data to backend

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,25 @@ import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
 function App() {
+  // This func can be made inside AddJobPage also
+  const addJob = async (newJob) => {
+    // console.log(newJob);
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return res;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path='/job/:id' element={<JobPage />} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const AddJobPage = () => {
+const AddJobPage = ({ addJobSubmit }) => {
 
     const [title, setTitle] = useState('');
     const [type, setType] = useState('Full-Time');
@@ -11,6 +12,8 @@ const AddJobPage = () => {
     const [companyDescription, setCompanyDescription] = useState('');
     const [contactEmail, setContactEmail] = useState('');
     const [contactPhone, setContactPhone] = useState('');
+
+    const navigate = useNavigate();
 
     const submitForm = (e) => {
         e.preventDefault();
@@ -29,6 +32,8 @@ const AddJobPage = () => {
             },
         };
         console.log(newJob);
+        addJobSubmit(newJob);
+        return navigate('/jobs');
     }
 
 
